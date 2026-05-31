d461218bd023ae66216fe6b0f14d8346efe8eea3
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-05-31 17:27:10 +0100

    Make Next.js build stable on Railway

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    cpus: 1
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' }
