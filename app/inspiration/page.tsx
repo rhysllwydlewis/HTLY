fca34ad461eb37634cacd6c207e13ac1712ba693
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-05-31 17:46:11 +0100

    Add inspiration page

---
+++ app/inspiration/page.tsx
@@ -0,0 +1,5 @@
+import { PageShell } from '@/components/PageShell';
+
+export default function Page() {
+  return <PageShell title="Inspiration" text="Travel ideas and guides will be expanded from this route." />;
+}
