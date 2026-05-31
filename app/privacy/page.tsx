fa73ad82d473b6ef847dccc827acb88a7b808d27
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-05-31 20:13:50 +0100

    Fix homepage follow-up quality issues

---
+++ app/privacy/page.tsx
@@ -0,0 +1,5 @@
+import { PageShell } from '@/components/PageShell';
+
+export default function PrivacyPage() {
+  return <PageShell title="Privacy Policy" text="HTLY privacy information will be added here." />;
+}
---
+++ app/terms/page.tsx
@@ -0,0 +1,5 @@
+import { PageShell } from '@/components/PageShell';
+
+export default function TermsPage() {
+  return <PageShell title="Terms & Conditions" text="HTLY booking and website terms will be added here." />;
+}
