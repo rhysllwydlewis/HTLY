892c27fc9f293529ffe703142db2dc2d13c483a2
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-05-31 17:45:35 +0100

    Add destinations route

---
+++ app/destinations/page.tsx
@@ -0,0 +1,5 @@
+import { PageShell } from '@/components/PageShell';
+
+export default function DestinationsPage() {
+  return <PageShell title="Destinations" text="Destination guides and holiday collections will be expanded from this route." />;
+}
