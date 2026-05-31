1c0f4caaed1a0fa313bb8dc2f9e7dc6e17a0d5e5
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-05-31 17:13:13 +0100

    Add brand configuration

---
+++ lib/brand.ts
@@ -0,0 +1,15 @@
+export type BrandConfig = {
+  name: string;
+  suffix: string;
+  tagline: string;
+  siteUrl: string;
+};
+
+export const brand: BrandConfig = {
+  name: process.env.NEXT_PUBLIC_BRAND_NAME || 'HTLY',
+  suffix: process.env.NEXT_PUBLIC_BRAND_SUFFIX || 'co.uk',
+  tagline: process.env.NEXT_PUBLIC_BRAND_TAGLINE || 'HOLIDAY DEALS MADE EASY',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://htly.co.uk'
+};
+
+export const brandDisplay = `${brand.name}.${brand.suffix}`;
