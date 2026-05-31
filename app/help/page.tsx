19c04e60b55446d0e265141f6650ef8501913278
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-05-31 17:47:18 +0100

    Add help route

---
+++ app/help/page.tsx
@@ -0,0 +1,5 @@
+import { PageShell } from '@/components/PageShell';
+
+export default function Page() {
+  return <PageShell title="Help" text="Help content will be expanded from this route." />;
+}
